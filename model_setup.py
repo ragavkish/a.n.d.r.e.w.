7a19d6bc427604e62bd3be77844aa9ffb1f8d322
ragavkish <ragavkishoredm@gmail.com>
2024-12-26 17:50:26 +0530

    duplicate delete

--- model_setup.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-model_name = "models\\anderson"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
-model.eval()
