20afe16e1b80d71b88a2c10d09af1cb310ab2303
ragavkish <ragavkishoredm@gmail.com>
2024-12-30 14:31:43 +0530

    improvizing

--- server_side/input_response.py
+++ server_side/input_response.py
@@ -2,9 +2,27 @@ from transformers import pipeline
 
 paraphraser = pipeline("text2text-generation", model="t5-small")
 
-initial_prompts = ["hey I am A.N.D.R.E.W.!", "hi I am A.N.D.R.E.W.!", "hello I am A.N.D.R.E.W.!", "Hey I am A.N.D.R.E.W.!", "Hellow this is A.N.D.R.E.W.!", "A.N.D.R.E.W. reporting!", "Hi! You're speaking to A.N.D.R.E.W.", "Heyy! A.N.D.R.E.W. here!"]
-
-trigger_inputs = ["hi", "hey", "hello", "Hello", "Hi", "Hey", "Hallo", "Hola"]
+initial_prompts = [
+    "hey I am A.N.D.R.E.W.!", 
+    "hi I am A.N.D.R.E.W.!", 
+    "hello I am A.N.D.R.E.W.!", 
+    "Hey I am A.N.D.R.E.W.!", 
+    "Hellow this is A.N.D.R.E.W.!", 
+    "A.N.D.R.E.W. reporting!", 
+    "Hi! You're speaking to A.N.D.R.E.W.", 
+    "Heyy! A.N.D.R.E.W. here!", 
+    "Hey, it's A.N.D.R.E.W. here!", 
+    "Hello, you're chatting with A.N.D.R.E.W.!", 
+    "A.N.D.R.E.W. at your service!", 
+    "Hey! A.N.D.R.E.W. speaking!", 
+    "Greetings, this is A.N.D.R.E.W.!", 
+    "Hi there! A.N.D.R.E.W. reporting!", 
+    "What's up? A.N.D.R.E.W. here!", 
+    "A.N.D.R.E.W. in the house, what's good?", 
+    "Yo! It's A.N.D.R.E.W.!", 
+    "Hello, you're speaking to A.N.D.R.E.W. now!"
+]
+trigger_inputs = ["hi", "hey", "hello", "Hello", "Hi", "Hey", "Hallo", "Hola", "Greetings", "Salutations", "Howdy", "Yo", "What's up", "Sup", "Ahoy", "Shalom", "Ciao", "Bonjour", "Aloha", "Namaste", "Salve", "Konnichiwa", "G'day", "Sawasdee", "Wassup", "Hei", "Holla", "Hiyah"]
 
 target_responses = []
 
@@ -15,8 +33,8 @@ for prompt in initial_prompts:
         num_beams=5,
         num_return_sequences=5,
         do_sample=True,
-        top_k=50,
-        temperature=0.7,
+        top_k=40,
+        temperature=0.6,
     )
     for paraphrase in paraphrases:
         generated_text = paraphrase["generated_text"].strip()
