0995799a80258e93e92979a9bbca1a1d9256199c
ragavkish <ragavkishoredm@gmail.com>
2025-01-03 20:02:15 +0530

    modification

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
 from server_side.routes import init_routes
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
+app = Flask(__name__)
 
-    init_routes(app)
+init_routes(app)
 
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
+if __name__ == "__main__":
     app.run(debug=True)
