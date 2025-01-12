a5566e45ecb1febd4cbb01e835c10a65ef6df49f
ragavkish <ragavkishoredm@gmail.com>
2025-01-12 11:59:13 +0530

    google_signup setup

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from flask_mail import Mail
+from flask_dance.contrib.google import make_google_blueprint
 from server_side.routes import init_routes
+import os
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
 
-init_routes(app)
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = os.getenv("APP_SECRET_KEY", "default-secret-key")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
+    app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
+    google_bp = make_google_blueprint(redirect_to="google_login")
+    app.register_blueprint(google_bp, url_prefix="/google_login")
+
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 587
+    app.config["MAIL_USE_TLS"] = True
+    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+    app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+    mail = Mail(app)
+
+    init_routes(app, mail)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)
