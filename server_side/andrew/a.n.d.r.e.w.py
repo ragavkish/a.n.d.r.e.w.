def330df45f713deec7fa17bb1b91faba719a2a8
ragavkish <ragavkishoredm@gmail.com>
2025-02-11 10:54:21 +0530

    andrew_modified

--- server_side/andrew/a.n.d.r.e.w.py
+++ server_side/andrew/a.n.d.r.e.w.py
@@ -4,7 +4,8 @@ import torch
 MODEL_PATH = "Z:/kizX/dataset/andrew/models/mistral_finetuned"
 CACHE_PATH = "Z:/kizX/datasets/cache"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, cache_dir=CACHE_PATH)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, cache_dir=CACHE_PATH, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
 model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, cache_dir=CACHE_PATH)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,14 +22,14 @@ def chat():
             break
 
         chat_history.append(user_input)
-        input_text = " ".join(chat_history[-5:])
+        input_text = "\n".join(chat_history[-5:])
 
         input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
 
         with torch.no_grad():
             output = model.generate(
                 input_ids,
-                max_length=input_ids.shape[-1] + 50,
+                max_length=min(1024, input_ids.shape[-1] + 50),
                 pad_token_id=tokenizer.eos_token_id,
                 do_sample=True,
                 temperature=0.7,
@@ -36,7 +37,7 @@ def chat():
                 top_p=0.9
             )
 
-        response = tokenizer.decode(output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)        
+        response = tokenizer.decode(output[:, input_ids.shape[-1]:][0], skip_special_tokens=True).strip()
         chat_history.append(response)
         print(f"A.N.D.R.E.W.: {response}")
 
